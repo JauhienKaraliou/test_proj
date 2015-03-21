05cb835857405f8a5a8a4905dc3b2e783fb15a12
JauhienKaraliou <jauhien.karaliou@gmail.com>
2015-03-21 10:42:25 +0300

    minor fixes

--- src/JK/WebsiteBundle/Controller/IndexController.php
+++ src/JK/WebsiteBundle/Controller/IndexController.php
@@ -4,11 +4,21 @@ namespace JK\WebsiteBundle\Controller;
 
 use JK\CommonBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
-    public function indexAction($name)
+    private $templating;
+
+    public function __construct($templating)
     {
-        return $this->render('JKWebsiteBundle:Default:index.html.twig', array('name' => $name));
+        $this->templating = $templating;
+    }
+    public function indexAction($name='Jauhien')
+    {
+        return $this->templating->renderResponse(
+            'JKWebsiteBundle:Default:index.html.twig',
+            array('name' => $name));
+        //return $this->render('JKWebsiteBundle:Default:index.html.twig', array('name' => $name));
     }
 }
